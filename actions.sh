0a47b2f6e7019f1047f04526fcc5a3ac0dc9b6f9
MFredX <47466168+MFredX@users.noreply.github.com>
2021-12-13 15:25:17 +1100

    replace > with cat

--- actions.sh
+++ actions.sh
@@ -2,4 +2,4 @@ python --version
 echo Running version fetching script
 cat mta.yaml 
 echo --------Printing Version---------
-<  mta.yaml shyaml get-value 'version'
+cat mta.yaml | shyaml get-value 'version'
