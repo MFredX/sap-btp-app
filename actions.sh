324ab8785874f0df54132af74bcf55adb7902047
MFredX <47466168+MFredX@users.noreply.github.com>
2021-12-13 15:22:51 +1100

    adding '' to version instead of version

--- actions.sh
+++ actions.sh
@@ -2,25 +2,4 @@ python --version
 echo Running version fetching script
 cat mta.yaml 
 echo --------Printing Version---------
-<  mta.yaml shyaml get-value version 
-
-cat <<EOF > test.yaml
-name: "MyName !!"
-subvalue:
-    how-much: 1.1
-    how-many: 2
-    things:
-        - first
-        - second
-        - third
-    maintainer: "Valentin Lab"
-    description: |
-        Multiline description:
-        Line 1
-        Line 2
-subvalue.how-much: 1.2
-subvalue.how-much\more: 1.3
-subvalue.how-much\.more: 1.4
-EOF
-
-cat test.yaml | shyaml get-value name
+<  mta.yaml shyaml get-value 'version'
