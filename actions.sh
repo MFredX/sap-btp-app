eae8a60419d87a43232469ad55aa645b399fbe1e
MFredX <47466168+MFredX@users.noreply.github.com>
2021-12-13 14:13:54 +1100

    commenting added

--- actions.sh
+++ actions.sh
@@ -1,4 +1,4 @@
-echo Running version fetching script
-shyaml -V | grep "^libyaml used:"  
+echo Running version fetching script-------------------
+#shyaml -V | grep "^libyaml used:"  
 cat mta.yaml | shyaml get-value version
 
