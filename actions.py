9bdbae24f9ed5b4766b03d2125ca2cd1ceb72a71
MFredX <47466168+MFredX@users.noreply.github.com>
2021-12-14 12:06:52 +1100

    Changing appName to appID

--- actions.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-with open("mta.yaml", "r") as stream:
-    try:
-        parsed_yaml = yaml.safe_load(stream)
-        print(parsed_yaml["version"])
-    except yaml.YAMLError as exc:
-        print(exc)
--- actions.sh
+++
@@ -1,5 +0,0 @@
-python --version
-echo Running version fetching script
-cat mta.yaml 
-echo --------Printing Version---------
-cat mta.yaml | shyaml get-value 'version'
