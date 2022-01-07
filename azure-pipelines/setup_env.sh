4b45c4f5c6fc23437882754f3ced3c3c81ffea15
MFredX <47466168+MFredX@users.noreply.github.com>
2022-01-07 15:44:04 +1100

    Reading mta ID and appVersion from build pipeline

--- azure-pipelines/setup_env.sh
+++ azure-pipelines/setup_env.sh
@@ -19,10 +19,35 @@ then
 elif [ $BRANCH = "test" ]
 then
     echo "Configuring test environment"
+    echo "Configuring dev environment"
+    echo "Deleting mta.yaml,ui5-deploy.yaml,xs-security.json,webapp/manifest.json......"
+    rm mta.yaml
+    rm ui5-deploy.yaml
+    rm xs-security.json
+    rm webapp/manifest.json
+    echo "Deletion complete..."
+    echo "Moving env specific files..."
+    cp env-properties/test/test-mta.yaml mta.yaml
+    cp env-properties/test/test-ui5-deploy.yaml ui5-deploy.yaml 
+    cp env-properties/test/test-xs-security.json xs-security.json 
+    cp env-properties/test/test-manifest.json webapp/manifest.json
+    echo "Environment setup complete..."
 
 elif [ $BRANCH = "master" ]
 then
     echo "Configuring prod environment"
+    echo "Configuring dev environment"
+    echo "Deleting mta.yaml,ui5-deploy.yaml,xs-security.json,webapp/manifest.json......"
+    rm mta.yaml
+    rm ui5-deploy.yaml
+    rm xs-security.json
+    rm webapp/manifest.json
+    echo "Deletion complete..."
+    echo "Moving env specific files..."
+    cp env-properties/prod/prod-mta.yaml mta.yaml
+    cp env-properties/prod/prod-ui5-deploy.yaml ui5-deploy.yaml 
+    cp env-properties/prod/prod-xs-security.json xs-security.json 
+    cp env-properties/prod/prod-manifest.j
 
 else
     echo "No suitable env found"
