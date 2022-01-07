099fe5bf7592a8e2247240683413462102d00276
MFredX <47466168+MFredX@users.noreply.github.com>
2022-01-07 11:17:55 +1100

    Added shell script

--- azure-pipelines/setup_env.sh
+++ azure-pipelines/setup_env.sh
@@ -1,2 +1,18 @@
+#!/bin/sh
+
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 echo $branch
+echo "------------"
+if [ $branch == "dev" ]
+then
+    echo "Configuring dev environment"
+fi
+elif [ $branch == "test" ]
+then
+    echo "Configuring test environment"
+elif [ $branch == "master" ]
+then
+    echo "Configuring prod environment"
+else
+    echo "No suitable env found"
+fi
