b7678d35465d7f7310dd6e9adc69b0754e50df96
MFredX <47466168+MFredX@users.noreply.github.com>
2022-01-07 15:03:09 +1100

    setup_env.sh modfied to work with Azure Pipelines task

--- azure-pipelines/setup_env.sh
+++ azure-pipelines/setup_env.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 BRANCH=$BUILD_SOURCEBRANCHNAME
-echo $BRANCH
 if [ $BRANCH = "dev" ]
 then
     echo "Configuring dev environment"
-    echo "Deleting mta.yaml,ui5-deploy.yaml,xs-security.json,webapp/manifest.json...."
+    echo "Deleting mta.yaml,ui5-deploy.yaml,xs-security.json,webapp/manifest.json......"
     rm mta.yaml
     rm ui5-deploy.yaml
     rm xs-security.json
     rm webapp/manifest.json
-    echo "Delete complete"
-    echo "Moving env specific files"
+    echo "Deletion complete..."
+    echo "Moving env specific files..."
     cp env-properties/dev/dev-mta.yaml mta.yaml
     cp env-properties/dev/dev-ui5-deploy.yaml ui5-deploy.yaml 
     cp env-properties/dev/dev-xs-security.json xs-security.json 
     cp env-properties/dev/dev-manifest.json webapp/manifest.json
+    echo "Environment setup complete..."
 
 elif [ $BRANCH = "test" ]
 then
