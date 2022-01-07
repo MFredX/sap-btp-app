1f440c5364e918154e40e52d18f3be2cbe8dec19
MFredX <47466168+MFredX@users.noreply.github.com>
2022-01-07 14:44:35 +1100

    Made unix friendly line endings for setup_env.sh script and pipeline references file via task.

--- azure-pipelines/setup_env.sh
+++ azure-pipelines/setup_env.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
-
-branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-echo $branch
-echo "------------"
-if [ $branch == "dev" ]
+BRANCH=$(Build.SourceBranchName)
+if [ $BRANCH = "dev" ]
 then
     echo "Configuring dev environment"
-fi
-elif [ $branch == "test" ]
+    echo "Deleting mta.yaml,ui5-deploy.yaml,xs-security.json,webapp/manifest.json...."
+    rm mta.yaml
+    rm ui5-deploy.yaml
+    rm xs-security.json
+    rm webapp/manifest.json
+    echo "Delete complete"
+    echo "Moving env specific files"
+    cp env-properties/dev/dev-mta.yaml mta.yaml
+    cp env-properties/dev/dev-ui5-deploy.yaml ui5-deploy.yaml 
+    cp env-properties/dev/dev-xs-security.json xs-security.json 
+    cp env-properties/dev/dev-manifest.json webapp/manifest.json
+
+elif [ $BRANCH = "test" ]
 then
     echo "Configuring test environment"
-elif [ $branch == "master" ]
+elif [ $BRANCH = "master" ]
 then
     echo "Configuring prod environment"
 else
